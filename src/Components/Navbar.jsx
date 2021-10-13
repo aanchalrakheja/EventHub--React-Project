5a413b4ab6c9001c91b3bfd5d583aef5ea624d77
aanchalrakheja <aanchalrakheja2001@gmail.com>
2021-10-13 23:16:37 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,15 +8,23 @@ function Navbar()
     return(
         <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand">EventHub</a>
+          <button onClick={()=>{
+                  window.location.href="/";
+                }} className="navbar-brand">EventHub</button>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <div className="navbar-nav">
-                <a className="nav-link" href="/">Browse Events</a>
-                <a className="nav-link" href="./likedEvents">Liked Events</a>
-                <a className="nav-link" href="./hostevent">Host an Event</a>
+                <button onClick={()=>{
+                  window.location.href="/";
+                }} className="nav-link">Browse Events</button>
+                <button onClick={()=>{
+                  window.location.href="./likedEvents";
+                }} className="nav-link">Liked Events</button>
+                <button onClick={()=>{
+                  window.location.href="./hostEvent";
+                }} className="nav-link">Host an Event</button>
               </div>
               <div className="profile">
                 <img src={profileImg} alt="profile-img"/>
